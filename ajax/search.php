39b04fcdce86bfc789410a6ae31f44ddb506c6cf
Tim Buckingham <mr@timbuckingham.com>
2016-09-01 15:38:56 -0400

    2.0 release

--- ajax/search.php
+++ ajax/search.php
@@ -1,28 +1,39 @@
-<?
+<?php
 	$pages = $admin->searchPages($_POST["query"]);
 	$resources = $admin->searchResources($_POST["query"]);
 
 	if (count($pages)) {
 ?>
 <div>Pages</div>
-<?
+<?php
 		foreach ($pages as $page) {
-			// Get the parent so we can provide some context to where this page lives
-			$parent = $cms->getPage($page["parent"],false);
+			if ($page["parent"] > 0) {
+				// Get the parent so we can provide some context to where this page lives
+				$parent = $cms->getPage($page["parent"],false);
+				$text = $parent["nav_title"]."&nbsp;&raquo;&nbsp;".$page["nav_title"];
+			} else {
+				$text = $page["nav_title"];
+			}
 ?>
-<a href="<?=WWW_ROOT.$page["path"]?>/" title="<?=$page["title"]?>"><?=($parent["id"] ? $parent["nav_title"] : "Home")?>&nbsp;&nbsp;&raquo;&nbsp;&nbsp;<?=$page["nav_title"]?></a>
-<?
+<a href="<?=WWW_ROOT.$page["path"]?>/" data-placeholder="Page: <?=$text?>"><?=$text?></a>
+<?php
 		}
 	}
 
-	if (count($resources)) {
+	if (count($resources["resources"])) {
 ?>
-<div>Resources</div>
-<?
+<div>Files</div>
+<?php
 		foreach ($resources["resources"] as $resource) {
 ?>
-<a href="<?=str_ireplace("{staticroot}", STATIC_ROOT, $resource["file"])?>" title="<?=$resource["name"]?>"><?=$resource["name"]?></a>
-<?
+<a href="<?=str_ireplace("{staticroot}", STATIC_ROOT, $resource["file"])?>" data-placeholder="File: <?=$resource["name"]?>"><?=$resource["name"]?></a>
+<?php
 		}
+	}
+
+	if (!count($pages) && !count($resources["resources"])) {
+?>
+<em>No Search Results</em>
+<?php
 	}
 ?>
--- field-types/link-finder/draw.php
+++ field-types/link-finder/draw.php
@@ -1,50 +1,74 @@
-<?
-	/*
-		When drawing a field type you are provided with the $field array with the following keys:
-			"title" — The title given by the developer to draw as the label (drawn automatically)
-			"subtitle" — The subtitle given by the developer to draw as the smaller part of the label (drawn automatically)
-			"key" — The value you should use for the "name" attribute of your form field
-			"value" — The existing value for this form field
-			"id" — A unique ID you can assign to your form field for use in JavaScript
-			"tabindex" — The current tab index you can use for the "tabindex" attribute of your form field
-			"options" — An array of options provided by the developer
-			"required" — A boolean value of whether this form field is required or not
-	*/
+<?php
+	$ipl_value = BigTreeAdmin::makeIPL($field["value"]);
+	$placeholder = $field["value"];
+	$show_value = false;
+
+	// See if it's a page
+	if (substr($ipl_value, 0, 6) == "ipl://") {
+		list($protocol, $empty, $id) = explode("/", $ipl_value);
+
+		// Get the page name for the placeholder
+		$page = BigTreeCMS::getPage($id, false);
+
+		if ($page["parent"]) {
+			$parent = BigTreeCMS::getPage($page["parent"], false);
+			$placeholder = "Page: ".$parent["nav_title"]."&nbsp;&nbsp;&raquo;&nbsp;&nbsp;".$page["nav_title"];
+		} else {
+			$placeholder = "Page: ".$page["nav_title"];
+		}
+	// It's a resource
+	} elseif (substr($ipl_value, 0, 6) == "irl://") {
+		list($protocol, $empty, $id) = explode("/", $ipl_value);
+
+		// Get resource to get it's name
+		$resource = BigTreeAdmin::getResource($id);
+		$placeholder = "File: ".$resource["name"];
+	} else {
+		$show_value = true;
+	}
 ?>
 <style>
-	#<?=$field["id"]?>_lf { position: relative; }
-	#<?=$field["id"]?>_lf_output { background: #FFF; border: 1px solid #AAA; box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15); max-height: 200px; overflow-y: auto; margin: -2px 0 0 0; position: absolute; width: 896px; z-index: 3; }
-	#<?=$field["id"]?>_lf_output div { background: #59A8E9; color: #FFF; font-size: 13px; height: auto; line-height: 20px; padding: 4px 10px 3px; }
-	#<?=$field["id"]?>_lf_output a { border-bottom: 1px solid #DDD; color: #333; display: block; font-size: 10px; height: auto; line-height: 14px; padding: 3px 10px; }
-	#<?=$field["id"]?>_lf_output a:nth-child(odd) { background: #FAFAFA; }
-	#<?=$field["id"]?>_lf_output a:last-child { border-bottom: none; }
-	#<?=$field["id"]?>_lf_output a:hover { background: #EEE; }
+	#<?=$field["id"]?>_results { background: #FFF; border: 1px solid #AAA; box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15); max-height: 200px; overflow-y: auto; margin: -2px 0 0 0; position: absolute; width: 896px; z-index: 3; }
+	#<?=$field["id"]?>_results div { background: #59A8E9; color: #FFF; font-size: 13px; height: auto; line-height: 20px; padding: 4px 10px 3px; }
+	#<?=$field["id"]?>_results a { border-bottom: 1px solid #DDD; color: #333; display: block; font-size: 10px; height: auto; line-height: 14px; padding: 3px 10px; }
+	#<?=$field["id"]?>_results a:nth-child(odd) { background: #FAFAFA; }
+	#<?=$field["id"]?>_results a:last-child { border-bottom: none; }
+	#<?=$field["id"]?>_results a:hover { background: #EEE; }
+	#<?=$field["id"]?>_results em { color: #333; display: block; font-size: 10px; line-height: 14px; padding: 3px 10px; }
+	#<?=$field["id"]?>_lf_results a:hover { background: #EEE; }
 </style>
-<div class="text_input" id="<?=$field["id"]?>_lf">
-	<input class="<?=$field["options"]["validation"]?>" type="text" tabindex="<?=$field["tabindex"]?>" name="<?=$field["key"]?>" value="<?=$field["value"]?>" id="<?=$field["id"]?>" />
-	<div id="<?=$field["id"]?>_lf_output" style="display: none;"></div>
+
+<div class="text_input">
+	<input type="hidden" name="<?=$field["key"]?>" value="<?=$field["value"]?>" id="<?=$field["id"]?>_value" />
+	<input class="<?=$field["options"]["validation"]?>" type="text" tabindex="<?=$field["tabindex"]?>" placeholder="<?=$placeholder?>"<?php if ($show_value) { ?> value="<?=$field["value"]?>"<?php } ?> id="<?=$field["id"]?>_query" />
+
+	<div id="<?=$field["id"]?>_results" style="display: none;"></div>
 </div>
+
 <script>
 	(function() {
-		var $field = $("#<?=$field["id"]?>");
-		var $output = $("#<?=$field["id"]?>_lf_output");
+		var ValueField = $("#<?=$field["id"]?>_value")
+		var QueryField = $("#<?=$field["id"]?>_query");
+		var Results = $("#<?=$field["id"]?>_results");
+		var ResultWidth = QueryField.outerWidth(true) - 2;
 
-		$field.on("keyup",function() {
-			var query = $field.val().trim(),
-				width = $field.outerWidth(true) - 2;
+		QueryField.on("keyup", function() {
+			var query = QueryField.val().trim();
 
-			$output.css({ width: width }).scrollTop(0);
+			Results.css({ width: ResultWidth }).scrollTop(0);
+			ValueField.val(query);
 
 			if (!query.length) {
-				$output.hide().html("");
+				Results.hide().html("");
 			} else {
-				$output.load("<?=ADMIN_ROOT?>*/com.fastspot.link-finder-field-type/ajax/search/", { query: query }, function() {
-					$output.show().children("a").click(function(e) {
-						e.preventDefault();
-						e.stopPropagation();
+				Results.load("<?=ADMIN_ROOT?>*/com.fastspot.link-finder-field-type/ajax/search/", { query: query }, function() {
+					Results.show().children("a").click(function(ev) {
+						ev.preventDefault();
+						ev.stopPropagation();
 
-						$field.val($(this).attr("href"));
-						$output.hide().html("");
+						ValueField.val($(this).attr("href"));
+						QueryField.val("").attr("placeholder", $(this).attr("data-placeholder"));
+						Results.hide().html("");
 					});
 				});
 			}
